86929f0a451b1114295f0f72227818c3a56cca0b
Cristiano <cristiano.tarconi@gmail.com>
2020-12-22 17:49:01 +0100

    js logic basic completed

--- js/script.js
+++ js/script.js
@@ -13,25 +13,70 @@ con difficoltà 0 => tra 1 e 100
 con difficoltà 1 =>  tra 1 e 80
 con difficoltà 2 => tra 1 e 50 */
 
-//Faccio generare al computer 16 numeri casuali che non devono ripetersi
+// Faccio generare al computer 16 numeri casuali che non devono ripetersi
 var bombs = [];
-var g = true;
 
 while (bombs.length < 5) { //!NUMERO TEST - DA CAMBIARE!
-    var number = randomizer(1, 25); //!NUMERO TEST - DA CAMBIARE!
+    var bombsNumber = randomizer(1, 25); //!NUMERO TEST - DA CAMBIARE!
 
-    if (bombs.includes(number) == false){
-        bombs.push(number);
+    if (bombs.includes(bombsNumber) == false) {
+        bombs.push(bombsNumber);
     }
 }
 
 console.log(bombs);
 
-// Creo prompt per utente dove inserirà i numeri per giocare
+//Creo un contenitore di riferimento per semplificare selezioni sbagliate nel prompt successivo
+var arrRif = numContainer(1, 25);
 
+// Creo prompt per utente dove inserirà i numeri per giocare
+// Array contenente numeri inseriti dal giocatore
+var userNumberArr = [];
+// Variabile sentinella 
+var sentinel = true;
+var message;
+
+while (userNumberArr.length < 20 && sentinel) { //!NUMERO TEST!
+    var userNumber = parseInt(prompt('Inserisci un numero:'));
+
+    if (arrRif.includes(userNumber) == false) {
+
+        alert('ATTENZIONE! Numero non valido!');
+
+    } else if (userNumberArr.includes(userNumber) == true) {
+
+        alert('Il numero è stato già utilizzato! Riprova');
+
+    } else if (bombs.includes(userNumber) == true) {
+
+        sentinel = false;
+
+    } else {
+
+        userNumberArr.push(userNumber);
+    }
+
+}
+
+if (userNumberArr.length == 20) {
+    alert('COMPLIMENTI!! HAI VINTO!');
+} else {
+    alert('Hai colpito una mina! Il tuo punteggio è di: ' + (userNumberArr.length - 1) + ' pt.');
+    console.log('Hai colpito una mina! Il tuo punteggio è di: ' + (userNumberArr.length - 1) + ' pt.');
+}
+
+/* console.log(userNumberArr); */
 
 // - FUNZIONI
-//Genera numeri random (min e max inclusi)
+// Genera numeri random (min e max inclusi)
 function randomizer(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
+}
+
+function numContainer(min, max) {
+    var arrContainer = [];
+    for (var i= min; i <= max; i++) {
+        arrContainer.push(i);
+    }
+    return arrContainer
+}
